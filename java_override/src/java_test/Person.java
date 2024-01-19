834d5fbdf0955a5fefb3907bcb0774083be02a25
601-16 <601-16@DESKTOP-J2TK4C1>
2024-01-19 17:42:11 +0900

    수정사항 반영하기.

--- java_override/src/java_test/Person.java
+++ java_override/src/java_test/Person.java
@@ -2,8 +2,6 @@ package java_test;
 
 class Person {
 	private String name;
-	private int[] gambleArr = new int[3];
-	private int sameNum;
 	
 	public void setName(String name) {
 		this.name = name;
@@ -11,40 +9,5 @@ class Person {
 	
 	public String getName() {
 		return name;
-	}
-	
-	public int getSameNum() {
-		return sameNum;
-	}
-	
-	public void gambling(Person person) {
-		System.out.println("[" + name + "]:"); 
-		for(int i : gambleArr) {
-			i = (int)(Math.random()*3 + 1);
-			System.out.print(i + " ");
-		}
-		
-		for(int i = 0; i < gambleArr.length ; i++) {
-			if(i == 0) {
-				if(gambleArr[0] == gambleArr[2]) {
-					sameNum += 1;
-				}				 
-				continue;
-			}
-			
-			if(gambleArr[i] == gambleArr[i-1]) {
-				sameNum += 1;
-			}			
-		}
-		
-		if(sameNum != 3) {
-			System.out.println("아쉽군요!");
-			return;
-		}			
-		else {
-			System.out.println("님이 이겼습니다.");
-			return;
-		}
-			
-	}
+	}	
 }
--- java_override/src/java_test/PersonMain.java
+++ java_override/src/java_test/PersonMain.java
@@ -1,15 +1,23 @@
 package java_test;
 
+import java.util.Scanner;
+
 class PersonMain {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		Person p1 = new Person();
 		Person p2 = new Person();
 		
-		while(true){
-			
+		System.out.print("1번째 선수 이름>>");
+		p1.setName(sc.next());
+		System.out.print("2번째 선수 이름>>");
+		p2.setName(sc.next());
+		Gambling gamble = new Gambling(); // 겜블 시작
+		while(gamble.getGameWin() == false){ // 승리 판단이 나올 때까지 반복
+			Person[] gamers = {p1,p2}; 
+			gamble.gambling(gamers); // 연산 과정이 길어서 따로 클래스로 빼서 연산 진행.
 		}
-
+		sc.close(); // Scanner 객체 사용 후 끝내기.
 	}
-
 }
