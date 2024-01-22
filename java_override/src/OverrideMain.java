9b3be55bc4420c9e61adcec4880b37cb43b0082b
601-16 <601-16@DESKTOP-J2TK4C1>
2024-01-22 17:32:57 +0900

    지저분하지만 어쩔수 없는 공부용 소스코드들

--- java_override/src/OverrideMain.java
+++ java_override/src/OverrideMain.java
@@ -1,74 +1,73 @@
-class Shape{
+
+class Shape {
 	public double getArea() {
 		return 0;
 	}
 }
 
-class Circle extends Shape{
+class Circle extends Shape {
 	double r;
+
 	public Circle(double r) {
 		this.r = r;
 	}
-	
+
 	@Override
 	public double getArea() {
-		return r*r*Math.PI;
+		return r * r * Math.PI;
 	}
 }
 
-class Rectangle extends Shape{
+class Rectangle extends Shape {
 	private int width, height;
-	
-	public Rectangle(int width, int height ) {
+
+	public Rectangle(int width, int height) {
 		this.width = width;
 		this.height = height;
 	}
-	
+
 	@Override
 	public double getArea() {
 		return width * height;
 	}
 }
 
-class Triangle extends Shape{
+class Triangle extends Shape {
 	private int width, height;
-	
-	public Triangle(int width, int height ) {
+
+	public Triangle(int width, int height) {
 		this.width = width;
 		this.height = height;
 	}
-	
+
 	@Override
 	public double getArea() {
-		return width * height/2;
+		return width * height / 2;
 	}
 }
 
-
-
 public class OverrideMain {
 	public static void main(String[] args) {
-		Shape[] shapeArr = {new Circle(100), new Rectangle(10, 10), new Triangle(10, 10)};
-		
+		Shape[] shapeArr = { new Circle(100), new Rectangle(10, 10), new Triangle(10, 10) }; 
 		double sum = 0;
-		for(Shape shape : shapeArr) {
-			sum = sum + shape.getArea();
+		for (Shape shape : shapeArr) {
+			sum = sum++;
+			shape.getArea();
+
 		}
-		
 		System.out.println(sum + "\n");
-		
-//		Circle c1 = new Circle(100);
-//		System.out.println(c1.getArea());
-//		
-//		Rectangle r1 = new Rectangle(10, 10);
-//		System.out.println(r1.getArea());
-//		
-//		Triangle r2 = new Triangle(10, 10);
-//		System.out.println(r2.getArea());
-//		
-//		double total = r1.getArea() + c1.getArea() + r2.getArea();
-//		System.out.println(total);
-		
-		
+		Circle c1 = new Circle(100);
+		System.out.println(c1.getArea());
+		Rectangle r1 = new Rectangle(10, 10);
+		System.out.println(r1.getArea());
+		Triangle r2 = new Triangle(10, 10);
+		System.out.println(r2.getArea());
+		double total = r1.getArea() + c1.getArea() + r2.getArea();
+		System.out.println(total);
+
+		Fruit fAry[] = { new Grape(), new Apple(), new Pear() };
+		for (Fruit f : fAry) {
+			f.print();
+		}
 	}
 }
